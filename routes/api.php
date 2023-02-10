9aa750dcace643982d103ffacd11453e39339df9
bijan biria <bijan.biria@gmail.com>
2023-02-10 12:15:42 +0330

    api created with full items test, repository, update model, api, request, resource, controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/is-working', [Controller::class, 'isWorking']);
+
+Route::group(['namespace' => '\App\Http\Controllers\Api'], function () {
+    Route::group(['prefix' => 'logs'], function () {
+        Route::get('/count', 'LogsController@getCount');
+    });
 });
